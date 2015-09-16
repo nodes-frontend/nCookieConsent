cc24d1071bd7d5c4d08839f2496da8c909fb54f0
Kris Maini <kris@Kriss-MacBook-Pro.local>
2015-09-16 10:26:50 +0200

    changed to controlleras in demo & do not bind top/bottom style - done with vanilla js instead in directive link

--- src/nCookieConsent.directive.js
+++ src/nCookieConsent.directive.js
@@ -20,15 +20,16 @@
 		};
 		return directive;
 
-		function link(scope, element) {
-			console.log(scope);
+		function link(scope, element, attrs) {
+			var e = document.querySelector('.n-cookie-consent');
+			console.log(e);
 			scope.consentHandler = scope.consentFunction();
 			switch (scope.position) {
 				case 'top':
-					scope.cookieStyle = {'top':0};
+					e.style.top = 0;
 					break;
 				case 'bottom':
-					scope.cookieStyle = {'bottom':0};
+					e.style.bottom = 0;
 					break;
 			}
 		}
